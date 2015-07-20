d24478bf8f044737d97821be1455273efe804728
Mahdi Tehrani <mahdi@frequencydesign.com>
2015-07-19 23:59:52 -0700

    added webpack

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,8 @@
-var gulp = require('gulp');
+var gulp = require("gulp");
+var gutil = require("gulp-util");
+var webpack = require("webpack");
+var WebpackDevServer = require("webpack-dev-server");
 
-gulp.task('default', function() {
+gulp.task("default", function() {
   // place code for your default task here
 });
